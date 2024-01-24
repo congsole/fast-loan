dfff1b342d6687f85a8519812e3acbf709ea5d9d
congsole <qax741@gmail.com>
2024-01-24 16:11:09 +0900

    first commit

---
+++ src/main/java/com/example/fastloan/FastLoanApplication.java
@@ -0,0 +1,13 @@
+package com.example.fastloan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastLoanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastLoanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fastloan/FastLoanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fastloan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastLoanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
